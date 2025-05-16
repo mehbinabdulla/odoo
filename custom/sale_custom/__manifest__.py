62babf69e5410b058c7033a731523227c4b851bb
cybrosys <mehbinabdullak@gmail.com>
2025-05-16 17:33:22 +0530

    May 16 2025

---
+++ custom/sale_custom/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Sales Custom Features',
+    'version': '18.0.1.1.0',
+    'summary': 'Custom Features for Odoo Sales Module',
+    'description': 'Custom Features for Odoo Sales Module',
+    'category': 'Sales',
+    'author': 'Mehbin Abdulla',
+    'depends': ['base','sale'],
+    'sequence':2,
+    'images': [
+        'static/description/icon.png'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/sale_order_views.xml',
+        'wizard/upload_sale_order_line_wizard_views.xml',
+    ],
+    'web_icon': 'static/description/icon.png',
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
